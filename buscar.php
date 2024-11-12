16ac71dcf864c33bff1939966d99cd2c244693a3
davi_shit-it <pitalitodavid654@gmail.com>
2024-11-12 09:27:50 -0500

    Paqueadero_F.1.1

--- buscar.php
+++ buscar.php
@@ -31,13 +31,13 @@ $vehiculosActivos = $parqueadero->listarVehiculosActivos();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parqueadero "El Aguacate" - Búsqueda</title>
+    <title>Parqueadero "El Aguacatal" - Búsqueda</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Parqueadero "El Aguacate"</a>
+            <a class="navbar-brand" href="#">Parqueadero "El Aguacatal"</a>
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav">
                     <li class="nav-item">
--- index.php
+++ index.php
@@ -27,13 +27,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parqueadero "El Aguacate" - Registro</title>
+    <title>Parqueadero "El Aguacatal" - Registro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Parqueadero "El Aguacate"</a>
+            <a class="navbar-brand" href="#">Parqueadero "El Aguacatal"</a>
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav">
                     <li class="nav-item">
